2484fd4ad4aec6da1767997ec52ad62db8f16bc6
mahm0udnasr <contact.mahmoudnasr@gmail.com>
2026-03-13 03:15:56 +0200

    Create Small Shopping Cart project

---
+++ small-shopping-cart.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        String ItemName;
+        double ItemPrice, total = 0;
+        int ItemCount;
+        Scanner item = new Scanner(System.in);
+        System.out.print("What item would you like to buy : ");
+        ItemName = item.nextLine();
+        System.out.print("What is the price for each      : ");
+        ItemPrice = item.nextDouble();
+        System.out.print("How many would you like         : ");
+        ItemCount = item.nextInt();
+        total = ItemPrice * ItemCount;
+        System.out.println("\nYou have bought " + ItemCount + " " + ItemName + "/s");
+        System.out.println("Your total is $" + total);
+        item.close();
+    }
+}
