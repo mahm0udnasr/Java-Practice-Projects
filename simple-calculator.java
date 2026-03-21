a44191e85708c43bbf7e2ba9d2bf6c01cce9bfa2
mahm0udnasr <contact.mahmoudnasr@gmail.com>
2026-03-21 09:07:07 +0200

    create simple calculator project

---
+++ simple-calculator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double num1, num2, result = 0;
+        char operator;
+        boolean validOperation = true;
+        System.out.print("Entre the first number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.print("Entre an operator (+, -, *, /, ^): ");
+        operator = scanner.next().charAt(0);
+
+        System.out.print("Entre the second number: ");
+        num2 = scanner.nextDouble();
+
+        switch (operator) {
+            case '+' -> result = num1 + num2;
+            case '-' -> result = num1 - num2;
+            case '*' -> result = num1 * num2;
+            case '/' -> {
+                if(num2 == 0) {
+                    System.out.println("Cannot divide on zero!");
+                    validOperation = false;
+                } else {
+                    result = num1 / num2;
+                }
+            }
+            case '^' -> result = Math.pow(num1, num2);
+            default -> {
+                System.out.println("Invalid operator!");
+                validOperation = false;
+            }
+        }
+        if(validOperation) {
+            System.out.println(result);
+        }
+        scanner.close();
+    }
+}
